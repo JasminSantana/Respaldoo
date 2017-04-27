d9f9ca00bbfc4d436c7a003d89ff52afecb7324f
Jasmin Santana <sant.mar.1997@gmail.com>
2017-04-26 21:29:55 -0500

    Add files via upload

---
+++ Geolocalizacion/WebContent/js/routes.js
@@ -0,0 +1,58 @@
+/**
+ * Examples using routes
+ */
+
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+
+function initialize(){
+	var directionsRerenderOptions = {
+			markerOptions:{
+				icon:new google.maps.MarkerImage("images/oil-Barrel-icon.png")
+			}
+	};
+	
+	directionsDisplay = new google.maps.DirectionsRenderer();
+	directionsDisplay.setOptions(directionsRerenderOptions);
+	var mexico_city = new google.maps.LatLng(19.434, -99.138);
+	var my_options = {
+			zoom: 5,
+			mapTypeId: google.maps.MapTypedId.ROADMAP,
+			center : mexico_city
+	};
+	
+	var map= new google.maps.Map(document.getElementById("map"), my_options);
+	
+	directionsDisplay.setMap(map);
+}
+
+function handleDirectionsResponse(start, end, directionsResult){
+	directionsDisplay.setDirections(directionsResult);
+	var routes = directionsResult.routes;
+	for(var i=0; i<routes.length; i++){
+		var totalDistance=0;
+		var legs = routes[i].legs;
+		for(j=0; j<legs.length; j++){
+			totalDistance += legs[j].distance.value;
+		}
+		document.getElementById("distance").innerHTML=Math.round(totalDistance/1000);
+	}
+}
+
+function calcRoute(){
+	var start=document.getElementById("start").value;
+	var end=document.getElementById("end").value;
+	var request={
+			origin: start,
+			destination: end,
+			travelMode: google.maps.DirectionsTravelMode.DRIVING
+	};
+	directionsService.route(request, 
+			function(directionsResult, status){
+		if(status==google.maps.DirectionsStatus.OK){
+			handleDirectionsResponse(
+					start, end, directionsResult);
+		}
+	});
+}
